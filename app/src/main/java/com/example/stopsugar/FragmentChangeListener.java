25e6109c7b3bebdfc8ff200325b0b81b1b29ce45
Martin Bouteiller <martin.bouteiller@etu.unice.fr>
2022-01-22 10:41:59 +0100

    Starting recipes display

---
+++ app/src/main/java/com/example/stopsugar/FragmentChangeListener.java
@@ -0,0 +1,9 @@
+package com.example.stopsugar;
+
+import androidx.fragment.app.Fragment;
+
+public interface FragmentChangeListener {
+
+    public void replaceFragment(Fragment fragment);
+
+}
---
+++ app/src/main/java/com/example/stopsugar/fragments/SelectRecipeFragment.java
@@ -0,0 +1,25 @@
+package com.example.stopsugar.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.stopsugar.R;
+
+public class SelectRecipeFragment extends Fragment {
+
+    private View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_recipe, container, false);
+
+        return rootView;
+    }
+}
