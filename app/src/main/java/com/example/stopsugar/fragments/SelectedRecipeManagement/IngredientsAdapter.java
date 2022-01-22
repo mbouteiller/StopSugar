333d9fd425bbb812a9678b51ed4bed7c66f7516d
Martin Bouteiller <martin.bouteiller@etu.unice.fr>
2022-01-22 22:48:22 +0100

    Full chocolate cake recipe

---
+++ app/src/main/java/com/example/stopsugar/fragments/SelectedRecipeManagement/IngredientsAdapter.java
@@ -0,0 +1,63 @@
+package com.example.stopsugar.fragments.SelectedRecipeManagement;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.stopsugar.R;
+
+import java.util.List;
+
+public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+
+    // data is passed into the constructor
+    public IngredientsAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycler_ingredient, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String ingredient = mData.get(position);
+        holder.myTextView.setText(ingredient);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.ingredientName);
+        }
+
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData.get(id);
+    }
+}
