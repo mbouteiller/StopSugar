05cd41f110962d6d63a41085029977c91454dddb
Martin Bouteiller <martin.bouteiller@etu.unice.fr>
2022-01-23 10:50:55 +0100

    Horizontal recyclers for recipes and chocolate cake click link

--- app/src/main/java/com/example/stopsugar/fragments/RecipeFragment.java
+++ app/src/main/java/com/example/stopsugar/fragments/RecipeFragment.java
@@ -9,14 +9,26 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.stopsugar.FragmentChangeListener;
 import com.example.stopsugar.R;
+import com.example.stopsugar.fragments.RecipesManagement.RecipeAdapter;
+
+import java.util.ArrayList;
 
 public class RecipeFragment extends Fragment {
 
     private View rootView;
     private Button buttonChoco;
+
+    private RecyclerView recyclerViewDeserts, recyclerViewStarters, recyclerViewMeals;
+    ArrayList<String> deserts, meals, starters;
+
+    RecyclerView.LayoutManager RecyclerViewLayoutManagerDeserts, RecyclerViewLayoutManagerMeals, RecyclerViewLayoutManagerStarters;
+    RecipeAdapter recipeAdapterDeserts, recipeAdapterMeals, recipeAdapterStarters;
+    LinearLayoutManager HorizontalLayoutDeserts, HorizontalLayoutMeals, HorizontalLayoutStarters;
 
     SelectRecipeFragment selectRecipeFragment = new SelectRecipeFragment();
 
@@ -28,19 +40,78 @@ public class RecipeFragment extends Fragment {
         buttonChoco =rootView.findViewById(R.id.buttonChoco);
         buttonChoco.setOnClickListener(chocoClick);
 
+        recyclerViewStarters = rootView.findViewById(R.id.starterRecycler);
+        recyclerViewMeals = rootView.findViewById(R.id.mealRecycler);
+        recyclerViewDeserts = rootView.findViewById(R.id.desertRecycler);
+
+        RecyclerViewLayoutManagerStarters = new LinearLayoutManager(getContext());
+        RecyclerViewLayoutManagerMeals = new LinearLayoutManager(getContext());
+        RecyclerViewLayoutManagerDeserts = new LinearLayoutManager(getContext());
+
+
+        // Set LayoutManager on Recycler View
+        recyclerViewStarters.setLayoutManager(RecyclerViewLayoutManagerStarters);
+        recyclerViewMeals.setLayoutManager(RecyclerViewLayoutManagerMeals);
+        recyclerViewDeserts.setLayoutManager(RecyclerViewLayoutManagerDeserts);
+
+        // Adding items to RecyclerView.
+        AddItemsToRecyclerViewArrayList();
+
+        // calling constructor of adapter
+        // with source list as a parameter
+        recipeAdapterStarters = new RecipeAdapter(starters);
+        recipeAdapterMeals = new RecipeAdapter(meals);
+        recipeAdapterDeserts = new RecipeAdapter(deserts);
+
+        // Set Horizontal Layout Manager
+        // for Recycler view
+        HorizontalLayoutStarters = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        HorizontalLayoutMeals = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        HorizontalLayoutDeserts = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewStarters.setLayoutManager(HorizontalLayoutStarters);
+        recyclerViewMeals.setLayoutManager(HorizontalLayoutMeals);
+        recyclerViewDeserts.setLayoutManager(HorizontalLayoutDeserts);
+
+        // Set adapter on recycler view
+        recyclerViewStarters.setAdapter(recipeAdapterStarters);
+        recyclerViewMeals.setAdapter(recipeAdapterMeals);
+        recyclerViewDeserts.setAdapter(recipeAdapterDeserts);
+
         return rootView;
     }
 
-    private View.OnClickListener chocoClick = new View.OnClickListener() {
+    public View.OnClickListener chocoClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             selectRecipe();
         }
     };
 
-    private void selectRecipe() {
+    public void selectRecipe() {
         FragmentChangeListener fc = (FragmentChangeListener) getActivity();
         assert fc != null;
         fc.replaceFragment(selectRecipeFragment);
+    }
+
+    public void AddItemsToRecyclerViewArrayList()
+    {
+        starters = new ArrayList<>();
+        starters.add("Salade César");
+        starters.add("Asperges au parmesan");
+        starters.add("Croissant jambon-fromage");
+        starters.add("Salade grecque");
+
+        meals = new ArrayList<>();
+        meals.add("Pavé de saumon");
+        meals.add("Steak tartare");
+        meals.add("Boeuf bourguignon");
+        meals.add("Lasagnes");
+
+        // Adding items to ArrayList
+        deserts = new ArrayList<>();
+        deserts.add("Mousse au chocolat");
+        deserts.add("Baba au rhum");
+        deserts.add("Galette des rois");
+        deserts.add("Crèpes");
     }
 }
---
+++ app/src/main/java/com/example/stopsugar/fragments/RecipesManagement/RecipeAdapter.java
@@ -0,0 +1,107 @@
+package com.example.stopsugar.fragments.RecipesManagement;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.stopsugar.FragmentChangeListener;
+import com.example.stopsugar.R;
+import com.example.stopsugar.activities.MainActivity;
+import com.example.stopsugar.fragments.RecipeFragment;
+import com.example.stopsugar.fragments.SelectRecipeFragment;
+
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    // List with String type
+    private List<String> list;
+    private RecipeFragment recipeFragment = new RecipeFragment();
+    private SelectRecipeFragment selectRecipeFragment = new SelectRecipeFragment();
+
+    // View Holder class which
+    // extends RecyclerView.ViewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Text View
+        TextView textView;
+
+        // parameterised constructor for View Holder class
+        // which takes the view as a parameter
+        public ViewHolder(View view)
+        {
+            super(view);
+
+            // initialise TextView with id
+            textView = view.findViewById(R.id.recipeNameText);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (textView.getText() == "Mousse au chocolat") {
+                        AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectRecipeFragment).commit();
+                    }
+                }
+            });
+        }
+    }
+
+    // Constructor for adapter class
+    // which takes a list of String type
+    public RecipeAdapter(List<String> horizontalList)
+    {
+        this.list = horizontalList;
+    }
+
+    // Override onCreateViewHolder which deals
+    // with the inflation of the card layout
+    // as an item for the RecyclerView.
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,
+                                         int viewType)
+    {
+
+        // Inflate item.xml using LayoutInflator
+        View itemView
+                = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.recycler_recipe,
+                        parent,
+                        false);
+
+        // return itemView
+        return new ViewHolder(itemView);
+    }
+
+    // Override onBindViewHolder which deals
+    // with the setting of different data
+    // and methods related to clicks on
+    // particular items of the RecyclerView.
+    @Override
+    public void onBindViewHolder(final ViewHolder holder,
+                                 final int position)
+    {
+
+        // Set the text of each item of
+        // Recycler view with the list items
+        holder.textView.setText(list.get(position));
+    }
+
+    // Override getItemCount which Returns
+    // the length of the RecyclerView.
+    @Override
+    public int getItemCount()
+    {
+        return list.size();
+    }
+}
