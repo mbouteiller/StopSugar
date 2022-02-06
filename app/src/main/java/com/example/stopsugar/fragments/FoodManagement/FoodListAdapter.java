e1a896107dacaaf0350579dda9ddcb411193a4f1
MetereauFrederic <frederic.metereau@etu.univ-cotedazur.fr>
2022-02-06 17:31:48 +0100

    Home Fragment, search, add, remove food from consommation

---
+++ app/src/main/java/com/example/stopsugar/fragments/FoodManagement/FoodListAdapter.java
@@ -0,0 +1,83 @@
+package com.example.stopsugar.fragments.FoodManagement;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.stopsugar.R;
+import com.example.stopsugar.fragments.HomeFragment;
+
+import java.util.List;
+
+public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private HomeFragment homeFragment;
+    private AlertDialog popup;
+
+    public FoodListAdapter(Context context, List<String> data, HomeFragment homeFragment, AlertDialog popup) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        this.homeFragment = homeFragment;
+        this.popup = popup;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        Button button;
+
+        ViewHolder(View view) {
+            super(view);
+            textView = view.findViewById(R.id.foodName);
+
+            button = view.findViewById(R.id.deleteButton);
+            button.setText("Ajouter");
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (textView.getText() == "Banane") {
+                        Toast.makeText(view.getContext(), "Ajout de l'aliment " + textView.getText(), Toast.LENGTH_SHORT).show();
+                        homeFragment.addFood(textView.getText().toString());
+                        homeFragment.changeImageStat(1);
+                        popup.cancel();
+                    } else {
+                        Toast.makeText(view.getContext(), "Vous n'ajoutez pas une Banane", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycler_food, parent, false);
+        return new FoodListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodListAdapter.ViewHolder holder, int position) {
+        String info = mData.get(position);
+        holder.textView.setText(info);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void filterList(List<String> filteredList) {
+        mData = filteredList;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/stopsugar/fragments/HomeFragment.java
+++ app/src/main/java/com/example/stopsugar/fragments/HomeFragment.java
@@ -1,17 +1,30 @@
 package com.example.stopsugar.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.stopsugar.R;
+import com.example.stopsugar.fragments.FoodManagement.FoodConsommationAdapter;
+import com.example.stopsugar.fragments.FoodManagement.FoodListAdapter;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -19,6 +32,12 @@ public class HomeFragment extends Fragment {
     private int[] sugarStats = new int[3];
     private int sugarState = 0;
     ImageView homeImage;
+
+    FoodConsommationAdapter foodConsommationAdapter;
+    FoodListAdapter foodListAdapter;
+
+    ArrayList<String> foodsConsommation;
+    ArrayList<String> foodsList;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,18 +51,98 @@ public class HomeFragment extends Fragment {
         sugarStats[2] = R.drawable.home_stat_2;
 
         addSugar.setOnClickListener(view -> {
-            if(sugarState < 2) {
-                sugarState++;
-                changeImageStat();
-            }
+            showPopUp();
         });
 
-        changeImageStat();
+        changeImageStat(sugarState);
+
+        configureRecyclerView();
 
         return rootView;
     }
 
-    private void changeImageStat() {
-        homeImage.setImageResource(sugarStats[sugarState]);
+    private void showPopUp() {
+        View popupView = LayoutInflater.from(this.getContext()).inflate(R.layout.popup_add_food, null);
+        AlertDialog.Builder popup = new AlertDialog.Builder(this.getContext());
+        popup.setView(popupView);
+        AlertDialog popUp = popup.create();
+
+        RecyclerView recyclerView = (RecyclerView) popupView.findViewById(R.id.foodSearchRecycler);
+        foodsList = new ArrayList<>();
+        foodsList.add("Banane");
+        foodsList.add("Fraises");
+        foodsList.add("Pizza");
+        foodsList.add("Gratin de pâtes");
+        foodsList.add("Avocat");
+        foodsList.add("Burger");
+        foodListAdapter = new FoodListAdapter(this.getContext(),foodsList, this, popUp);
+        recyclerView.setAdapter(foodListAdapter);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(popupView.getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        setSearchBarListener(popupView);
+
+        popUp.show();
+    }
+
+    private void setSearchBarListener(View view){
+        EditText searchPrograms = view.findViewById(R.id.food_search_bar);
+        searchPrograms.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+    }
+
+    private void filter(String text) {
+        ArrayList<String> filteredFoodList = new ArrayList<>();
+
+        for (String info : foodsList) {
+            if (info.toLowerCase().contains(text.toLowerCase())) {
+                filteredFoodList.add(info);
+            }
+        }
+        foodListAdapter.filterList(filteredFoodList);
+    }
+
+    public void changeImageStat(int sugarState) {
+        this.sugarState = sugarState;
+        homeImage.setImageResource(sugarStats[this.sugarState]);
+    }
+
+
+    private void configureRecyclerView() {
+        foodsConsommation = new ArrayList<>();
+
+        RecyclerView recyclerViewFood = rootView.findViewById(R.id.foodRecycler);
+        recyclerViewFood.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        foodConsommationAdapter = new FoodConsommationAdapter(this.getContext(), foodsConsommation, this);
+        recyclerViewFood.setAdapter(foodConsommationAdapter);
+    }
+
+    public void addFood(String foodName) {
+        foodsConsommation.add(foodName);
+        foodConsommationAdapter.notifyItemRangeInserted(foodsConsommation.size() - 1, foodsConsommation.size());
+    }
+
+    public void removeFood(String foodName) {
+        for (int i = 0; i < foodsConsommation.size(); i++) {
+            if (foodsConsommation.get(i).equals(foodName)) {
+                foodsConsommation.remove(i);
+                foodConsommationAdapter.notifyItemRemoved(i);
+                return;
+            }
+        }
     }
 }
